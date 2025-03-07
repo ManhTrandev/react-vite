84434cf25572f70b6652a8946c00638775899f4e
ManhTrandev <manhttran121@gmail.com>
2025-03-07 14:53:49 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,4 +1,12 @@
+import './style.css';
 const MyComponent = () => {
-  return <div>Welcome,bro</div>;
+  return (
+    <>
+      <div>Welcome,bro</div>
+      <div className="fragment" style={{ borderRadius: '10px' }}>
+        fragment
+      </div>
+    </>
+  );
 };
 export default MyComponent;
