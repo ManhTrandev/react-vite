b58f14ee84fe23e1dd57a469f3bbcce4e09e8399
ManhTrandev <manhttran121@gmail.com>
2025-03-07 14:39:38 +0700

    video 29

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,4 @@
+const MyComponent = () => {
+  return <div>Welcome,bro</div>;
+};
+export default MyComponent;
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,7 @@
+const SecondComponent = () => {
+  return <div>$$$$$$$$$$$$</div>;
+};
+const ThirdComponent = () => {
+  return <div>$$$$$$$$$$$$</div>;
+};
+export { SecondComponent, ThirdComponent };
