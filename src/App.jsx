3f0e5c6a2e0d4b286a4226df061a2400fd872766
ManhTrandev <manhttran121@gmail.com>
2025-03-08 03:06:01 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import TodoNew from './components/learn2/TodoNew';
 import './components/learn2/todo.css';
 import ReactLogo from './assets/react.svg';
 const App = () => {
+  const name = 'What your name';
+  const age = 25;
+  const data = {
+    address: 'HaNoi',
+    country: 'VietNam',
+  };
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData name={name} age={age} data={data} />
       <div className="todo-image">
         <img className="logo" src={ReactLogo} />
       </div>
--- src/components/learn2/TodoData.jsx
+++ src/components/learn2/TodoData.jsx
@@ -1,6 +1,9 @@
-const TodoData = () => {
+const TodoData = (props) => {
+  console.log('>>>Check prop', props);
+  const { name, age, data } = props;
   return (
     <div className="todo-data">
+      <div>See {name}</div>
       <div>Learn React</div>
       <div>Watch Youtude</div>
     </div>
