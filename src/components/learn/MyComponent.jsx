ef2f3dbd9106640f98cb97596f8655de4649aaca
ManhTrandev <manhttran121@gmail.com>
2025-03-07 15:07:42 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,11 +1,14 @@
 import './style.css';
 const MyComponent = () => {
+  //const tester = "Add ...";
+  const tester = [1, 2, 3];
   return (
     <>
-      <div>Welcome,bro</div>
+      <div> {JSON.stringify(tester)} and Welcome,bro</div>
       <div className="fragment" style={{ borderRadius: '10px' }}>
         fragment
       </div>
+      <div>{console.log('Magic')}</div>
     </>
   );
 };
