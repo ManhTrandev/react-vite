9902aceb9278c5c78861df23e0b4abf2c6e99f8e
ManhTrandev <manhttran121@gmail.com>
2025-03-08 15:39:48 +0700

    video 41

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,20 @@ const App = () => {
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew AddnewData={AddnewData} />
-      <TodoData todoList={todoList} />
-      <div className="todo-image">
-        <img className="logo" src={ReactLogo} />
-      </div>
+      {/* {todoList.length > 0 && <TodoData todoList={todoList} />}
+
+      {todoList.length === 0 && (
+        <div className="todo-image">
+          <img className="logo" src={ReactLogo} />
+        </div>
+      )} */}
+      {todoList.length > 0 ? (
+        <TodoData todoList={todoList} />
+      ) : (
+        <div className="todo-image">
+          <img className="logo" src={ReactLogo} />
+        </div>
+      )}
     </div>
   );
 };
