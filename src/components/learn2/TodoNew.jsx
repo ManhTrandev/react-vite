e755e423b7f6cf3b413f9a5d95e1c092b3fcdd10
ManhTrandev <manhttran121@gmail.com>
2025-03-08 13:36:43 +0700

    video 35

--- src/components/learn2/TodoNew.jsx
+++ src/components/learn2/TodoNew.jsx
@@ -2,10 +2,18 @@ const TodoNew = (props) => {
   console.log('>>>Check ', props);
   const { AddnewData } = props;
   // AddnewData('HIP');
+  const HandleClick = () => {
+    alert('Click me');
+  };
+  const HandleOnchange = (event) => {
+    console.log('>>>Check change', event.target.value);
+  };
   return (
     <div className="todo-new">
-      <input type="index" />
-      <button>Add</button>
+      <input type="index" onChange={HandleOnchange} />
+      <button style={{ cursor: 'pointer' }} onClick={HandleClick}>
+        Add
+      </button>
     </div>
   );
 };
